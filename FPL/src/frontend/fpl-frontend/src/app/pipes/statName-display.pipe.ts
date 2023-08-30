59c6e124fa339b65c1f1f35051b977ae7f82711c
thomarkey <thoma_cz@hotmail.com>
2023-08-30 16:25:22 +0200

    reworked statname enum and pipe for display purpose

--- FPL/src/frontend/fpl-frontend/src/app/pipes/statName-display.pipe.ts
+++ FPL/src/frontend/fpl-frontend/src/app/pipes/statName-display.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { getStatNameDisplayName } from '../models/statName.enum';
+import { getStatNameAbbreviation } from '../models/statName.enum';
 
 @Pipe({
     name: 'statNameDisplay'
 })
 export class StatNameDisplayPipe implements PipeTransform {
-    transform(value: any): string {
-        return getStatNameDisplayName(value);
+    transform(value: any, type: 'abbrev' | 'full' = 'full'): string {
+        if (type === 'abbrev') {
+            return getStatNameAbbreviation(value);
+        } else {
+            return getStatNameDisplayName(value);
+        }
     }
 }

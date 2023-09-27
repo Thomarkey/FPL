f1910c4eb7bc5513004751ba52ecff86bca0c800
thomarkey <thoma_cz@hotmail.com>
2023-09-27 11:59:16 +0200

    added teams with routing to their players and fixtures

--- FPL/src/frontend/fpl-frontend/src/app/components/teams-list/teams-list.component.ts
+++ FPL/src/frontend/fpl-frontend/src/app/components/teams-list/teams-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Team, getTeamDisplayName } from 'src/app/models/team.enum';
 
 @Component({
   selector: 'app-teams-list',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./teams-list.component.scss']
 })
 export class TeamsListComponent {
+
+  teams: Team[] = Object.values(Team);
+  getTeamDisplayName = getTeamDisplayName;
+  showLinksForTeam: string | null = null;
+
+
+  constructor() { }
+
+  toggleRowDetail(team: string) {
+    if (this.showLinksForTeam === team) {
+      this.showLinksForTeam = null;
+    } else {
+      this.showLinksForTeam = team;
+    }
+  }
 
 }
